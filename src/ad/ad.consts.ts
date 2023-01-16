531e10c4b435b31a6f8ec958bab184d0a4e79893
ProVitSer <prokinvs@gmail.com>
2023-01-16 22:40:16 +0300

    ref: Изменение регулярки по списку ad

--- src/ad/ad.consts.ts
+++ src/ad/ad.consts.ts
@@ -2,5 +2,5 @@ export const AD_GROUP = 'Удаленка';
 export const AD_ADD_ERROR = 'Ошибка добавление пользователя в AD группу';
 export const AD_DELETE_ERROR = 'Ошибка удаление пользователя из AD группу';
 export const AD_GET_USERS_ERROR = 'Ошибка в получение пользователей AD';
-export const REGEXP_AD_NAME = /name.*/;
-export const REGEXP_AD_UERNAME = /name.*:\s(.*)\r/gi;
+export const REGEXP_AD_NAME = /SamAccountName.*/;
+export const REGEXP_AD_UERNAME = /SamAccountName.*:\s(.*)\r/gi;
